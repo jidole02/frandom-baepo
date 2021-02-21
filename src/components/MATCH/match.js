7e591fe1b18caa9f87829f7d29d015678f1393d0
jiwon <jidole041214@naver.com>
2021-02-22 02:18:59 +0900

    ㄱㄱ

--- src/components/MATCH/match.js
+++ src/components/MATCH/match.js
@@ -8,6 +8,7 @@ const Match=()=>{
         <s.Background>
             <s.Title>FRANDOM!</s.Title>
             <Link style={LinkStyle} to="/chating"><s.MatchButton>랜덤채팅 시작!</s.MatchButton></Link>
+            <div id='mobonDivBanner_511407'><iframe name='ifrad' id='mobonIframe_511407' src='//www.mediacategory.com/servlet/adBanner?from="+escape(document.referrer)+"&s=511407&igb=75&iwh=468_60&cntad=1&cntsr=3' frameBorder='0' scrolling='no' style={{height:"60px", width:"468px",marginTop:"3%"}}></iframe></div>
         </s.Background>
     )
 }
