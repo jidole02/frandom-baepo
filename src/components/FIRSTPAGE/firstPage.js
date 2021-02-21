b766dbcba29cf07fbdd48e086f2da746a0f14065
jiwon <jidole041214@naver.com>
2021-02-22 02:31:45 +0900

    ㄱㄱ

--- src/components/FIRSTPAGE/firstPage.js
+++ src/components/FIRSTPAGE/firstPage.js
@@ -18,7 +18,7 @@ function FirstPage(Lang){
             <f.ButtonContainer style={{marginTop:"1%"}}>
                 <Link style={LinkStyle} to="/signup"><f.StartButton>{(a===0)?"GET START" : "회원가입"}</f.StartButton></Link> 
             </f.ButtonContainer>
-            <div id='mobonDivBanner_511407'><iframe name='ifrad' id='mobonIframe_511407' src='//www.mediacategory.com/servlet/adBanner?from="+escape(document.referrer)+"&s=511407&igb=75&iwh=468_60&cntad=1&cntsr=3' frameBorder='0' scrolling='no' style={{height:"60px", width:"468px",marginTop:"3%"}}></iframe></div>
+            <div id='mobonDivBanner_511410'><iframe name='ifrad' id='mobonIframe_511410' src='//www.mediacategory.com/servlet/adBanner?from="+escape(document.referrer)+"&s=511410&igb=75&iwh=640_107&cntad=1&cntsr=3' frameBorder='0' scrolling='no' style={{height:"107px", width:"640px",marginTop:"5%"}}></iframe></div>
         </f.FirstPageContainer>
     )
 }
--- src/components/MATCH/match.js
+++ src/components/MATCH/match.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import * as s from './styles'
-import {Link} from 'react-router-dom'
+import { useHistory} from 'react-router-dom'
 
 const Match=()=>{
-    const LinkStyle = { height:"6%",position:"absolute",marginBottom:"40%" }
+    const history = useHistory();
     return(
         <s.Background>
             <s.Title>FRANDOM!</s.Title>
-            <Link style={LinkStyle} to="/chating"><s.MatchButton>랜덤채팅 시작!</s.MatchButton></Link>
-            <div id='mobonDivBanner_511407'><iframe name='ifrad' id='mobonIframe_511407' src='//www.mediacategory.com/servlet/adBanner?from="+escape(document.referrer)+"&s=511407&igb=75&iwh=468_60&cntad=1&cntsr=3' frameBorder='0' scrolling='no' style={{height:"60px", width:"468px",marginTop:"3%"}}></iframe></div>
+            <s.MatchButton
+                onClick={()=>{
+                    history.push('/chating')
+                }}
+            >랜덤채팅 시작!</s.MatchButton>
+            <div id='mobonDivBanner_511410'><iframe name='ifrad' id='mobonIframe_511410' src='//www.mediacategory.com/servlet/adBanner?from="+escape(document.referrer)+"&s=511410&igb=75&iwh=640_107&cntad=1&cntsr=3' frameBorder='0' scrolling='no' style={{height:"107px", width:"640px",marginTop:"5%"}}></iframe></div>
         </s.Background>
     )
 }
