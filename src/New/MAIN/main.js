c3d990aa608ba49481883e22ef408a2d680a582d
jiwon <jidole041214@naver.com>
2021-02-24 15:23:18 +0900

    CREATE : 메인 구현

---
+++ src/New/MAIN/main.js
@@ -0,0 +1,29 @@
+import * as s from './styles'
+import {ReactComponent as 일러스트} from '../../ASSETS/일러스트1.svg'
+import {ReactComponent as 일러스트2} from '../../ASSETS/일러스트2.svg'
+
+export default function MainPage() {
+    return(
+        <>
+            <s.SvgContainer>
+                <일러스트/>
+                <일러스트2/>
+            </s.SvgContainer>
+            <s.MainContainer>
+                <s.IntroMent>
+                    깔끔한 랜덤채팅, 청결한 랜덤채팅. <br/>
+                    <b>FRANDOM</b>에서 만듭니다!
+                </s.IntroMent>
+                <s.SubIntro>랜덤채팅을 통해 다른 사람과 대화를 나누어 보세요!</s.SubIntro>
+                <s.BtnContainer>
+                    <s.Btn
+                        to="/chating"
+                    >시작하기 {">"}</s.Btn>
+                    <s.Btn
+                        to="/signup"
+                    >회원가입 {">"}</s.Btn>
+                </s.BtnContainer>
+            </s.MainContainer>
+        </>
+    )
+}
---
+++ src/New/MAIN/styles.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+import {mainColor} from '../../style/index'
+import {Link} from 'react-router-dom'
+
+export const SvgContainer = styled.div`
+    width:90%;
+    height:100%;
+    display:flex;
+    align-items:flex-end;
+    justify-content:space-between;
+    padding:0 5%;
+    position:absolute;
+    svg{
+        width:22%;
+    }
+`
+
+export const MainContainer = styled.div`
+    width:100%;
+    height:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    flex-direction:column;
+    position:absolute;
+    z-index:30;
+`
+
+export const IntroMent = styled.p`
+    color:black;
+    font-weight:bold;
+    text-align:center;
+    font-size:5vmin;
+    line-height:140%;
+    b{
+        color:${mainColor};
+    }
+`
+
+export const SubIntro = styled.p`
+    color:#9B9B9B;
+    font-size:1.8vmin;
+    margin-top:2%;
+`
+
+export const BtnContainer = styled.div`
+    width:22%;
+    height:6%;
+    display:flex;
+    justify-content:space-between;
+    margin-top:3%;
+`
+
+export const Btn = styled(Link)`
+    width:45%;
+    height:100%;
+    border:none;
+    border-radius:4px;
+    cursor:pointer;
+    background-color:transparent;
+    font-size:2vmin;
+    font-weight:bold;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    transition:0.5s;
+    :nth-of-type(1){
+        background-color:${mainColor};
+        color:white;
+    }
+    :last-of-type{
+        border:1px solid #BCBCBC;
+        color:#BCBCBC;
+    }
+    :hover{
+        box-shadow:3px 3px 10px #CFCCFF;
+    }
+`
