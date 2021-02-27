8580fb2bfb04192a72ecd15a44fb6016b8ccbdb5
jiwon <jidole041214@naver.com>
2021-02-27 11:22:52 +0900

    CREATE : 신고 모달

---
+++ src/New/CHATING/reportModal.js
@@ -0,0 +1,65 @@
+import * as s from './styles'
+
+import { useState } from 'react'
+
+import {Request} from '../../components/axios'
+
+export default function ReportModal(e) {
+    const [reportData,setReportData] = useState({
+        title:"",
+        cause:""
+    })
+    const Report =(e)=>{
+        const {name,value} = e.target;
+        setReportData({
+            ...reportData,
+            [name] : value
+        })
+    }
+    const ReportYou =()=>{
+        Request("POST", "v1/user/report",{"Content-type":"application/json", "Authorization":"Bearer " + window.localStorage.getItem("token")}, 
+        {
+            title : reportData.title,
+            description : reportData.cause
+        }).then(()=>{
+            alert("정상적으로 신고되었습니다.")
+            setReportData({
+                title:"",
+                cause:""
+            })
+        }).catch(()=>{
+            alert("신고 에러가 났습니다")
+            setReportData({
+                title:"",
+                cause:""
+            })
+        })
+    }
+
+    return(
+        <>
+           <s.ModalContainer>
+                <s.Report>
+                    <s.Alert>신고하기</s.Alert>
+                    <s.Hr/>
+                    <s.ReportInput
+                        placeholder="신고 제목을 입력해주세요."
+                        name="title"
+                        onChange={Report}
+                        value={reportData.title}
+                    ></s.ReportInput>
+                    <s.ReportInput
+                        placeholder="신고 제목을 입력해주세요."
+                        name="cause"
+                        onChange={Report}
+                        value={reportData.cause}
+                    ></s.ReportInput>
+                    <s.MBtnCont style={{width:"60%",height:"13%"}}>
+                        <s.MBtn onClick={e.event}>취소하기</s.MBtn>
+                        <s.MBtn onClick={ReportYou}>신고하기</s.MBtn>
+                    </s.MBtnCont>
+                </s.Report>
+            </s.ModalContainer> 
+        </>
+    )    
+}
